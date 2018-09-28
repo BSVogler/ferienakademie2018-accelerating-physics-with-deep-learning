fe122da632142c1daf7050466c95346df39aba39
BSVogler <mail@benediktsvogler.com>
2018-09-28 19:06:59 +0200

    missing part of c602f3e44a578527448e2d0ecf4604718ea95da3

--- tutorial.py
+++ tutorial.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from os import listdir
 from keras.callbacks import TensorBoard
 
+from visualization import vis
 
 # load dataset
 dataDir = "./data/trainSmallFA/"
@@ -23,15 +24,21 @@ for i, file in enumerate(files):
     ground_truth[i] = d[3:6]  # p, velx, vely
 # print("inputs shape = ", inputs.shape)
 
+
+# reorder because the channels msut be the last dimension
+inputs = inputs.transpose(0, 2, 3, 1)
+ground_truth = ground_truth.transpose(0, 2, 3, 1)
+
 # 80/20 split
 sizeTrain = int(len(files)*0.8)
+sizeValidation = int(len(files)-sizeTrain)
 print("size trainset: "+str(sizeTrain))
 train = inputs[0:sizeTrain]
 validation = inputs[sizeTrain:len(files)]
 
-train_ground_truth = ground_truth[0:sizeTrain]
-validation_ground_truth = ground_truth[sizeTrain:len(files)]
-
+# flatten data of item and 80/20 split
+train_ground_truth = ground_truth[0:sizeTrain].reshape((sizeTrain, -1))
+validation_ground_truth = ground_truth[sizeTrain:len(files)].reshape(sizeValidation, -1)
 
 def drawFigure():
     # show first file
@@ -87,18 +94,53 @@ def drawFigure():
 
 #%%
 
+
 # use sequential model
 model = keras.Sequential()
 
+#convolution filters
+f1 = 8
+f2 = 3
+#kernel size
+k1 = 4
+k2 = 2
+#stride
+s1 = 4
+s2 = 2
+#padding
 p1 = 0
 p2 = 0
 #output shape
 o1 = ((64 - k1) + 2*p1)/s1 + 1
+print(o1)
+o2 = ((o1 - k2) + 2*p2)/s2 + 1
+print(o2)
+
+model.add(keras.layers.Conv2D(input_shape = (64,64,3),
+                              filters = f1,
+                              kernel_size=(k1,k1),
+                              strides=(s1, s1),
+                              padding='valid',
+                              data_format = "channels_last",
+                             activation = 'tanh'))
+model.add(keras.layers.Conv2D(input_shape = (16,16,3),
+                              filters = f2,
+                              kernel_size=(k2,k2),
+                              strides=(s2, s2),
+                              padding='same',
+                              data_format = "channels_last",
+                             activation = 'tanh'))
+model.add(keras.layers.UpSampling2D(size=(4, 4), data_format="channels_last"))
+model.add(keras.layers.Flatten())
+
 # just one fully connected layer
 inputdim = 64*64*3
 layer1Dim = 12 * 12 * 3
 layer2Dim = 64 * 64 * 3
-model.add(keras.layers.Dense(layer1Dim))
+
+#model.add(keras.layers.Dense(layer1Dim,activation='relu'))
+model.add(keras.layers.Dense(layer1Dim,activation='tanh'))
+
 model.add(keras.layers.Dense(64 * 64 * 3))
 
 # ignores bias
@@ -116,37 +158,25 @@ def sizeof_fmt(num, suffix='B'):
 memory = numWeights * 4  # we are using four bytes for each weight
 
 print(str(numWeights) + " weights use " + str(sizeof_fmt(memory)))
+
 # configure the model
 model.compile(optimizer=tf.train.AdamOptimizer(0.0001), loss='mean_squared_error', metrics=['accuracy'])
 # AdamOptimizer(0.0006)
 
-# assign training data
-data_input = np.reshape(train[:], (len(train), -1))  # in general   np.reshape(inputs[0:i], (i,-1))
-data_ground_truth = np.reshape(train_ground_truth[:], (len(train_ground_truth), -1))
-
-print("data_input shape = ", data_input.shape)
-print("data_ground_truth shape = ", data_ground_truth.shape)
-
-# assign validation data
-# reshape so that we have in one dimension a batch
-val_input = np.reshape(validation[:], (len(validation), -1))
-val_ground_truth = np.reshape(validation_ground_truth[:], (len(validation_ground_truth), -1))
-
-print("val_input shape = ", val_input.shape)
-print("val_ground_truth shape = ", val_ground_truth.shape)
+#tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=False)
+#tensorboard = None
 
 # train the model
-model.fit(data_input, data_ground_truth, epochs=70, batch_size=10, validation_data=(val_input, val_ground_truth))
+history = model.fit(train, train_ground_truth, epochs=1, batch_size=50, validation_data=(validation, validation_ground_truth))
+
+
 
 # apply the model on the data
 k = 1
-predictions = model.predict(val_input[0:k, :], batch_size=1)
-truth = val_ground_truth[0:k, :]
+predictions = model.predict(validation[0:k, :], batch_size=1)
+truth = train_ground_truth[0:k, :]
 
 # print("predictions shape = ", predictions.shape)
-
-predictions = np.reshape(predictions, ((-1,) + train_ground_truth.shape[k:]))
-truth = np.reshape(truth, ((-1,) + train_ground_truth.shape[k:]))
 
 # print("predictions shape = ", predictions.shape)
 
@@ -214,5 +244,9 @@ def show_prediction(predictions):
     # [7] 'Difference x velocity'
     # [8] 'Difference y velocity'
 
+#%%
+predictions = predictions.reshape(len(predictions),64,64,3)
+truth = truth.reshape(len(truth),64,64,3)
 
-show_prediction(predictions)
+vis(predictions[0,:], truth[0,:])
+#show_prediction(predictions)
