added error distribution plotting function fixed relative error calculation script

functions.py
@@ -91,9 +91,9 @@ def plotter(x, y):
 
 def relative_error(truth,predictions):
     '''
-    :param truth: normalized ground truth, targets as [n_samples,64,64,3]
-    :param predictions: normalized output of network, predictions as [n_samples,64,64,3]
-    :return: relative error
+    :param truth: normalized ground truth, targets as [64,64,3]
+    :param predictions: normalized output of network, predictions as [64,64,3]
+    :return: relative error(scalar)
     '''
     results=np.sum(np.abs(predictions - truth)) / np.sum(np.abs(truth))
     return results
@@ -102,12 +102,25 @@ def relative_error_multiple(truth,predictions):
     '''
     :param truth: normalized ground truth, targets as [n_samples,64,64,3]
     :param predictions: normalized output of network, predictions as [n_samples,64,64,3]
-    :return: relative error
+    :return: relative error(array)
     '''
-    results=0*predictions
-    for i in range(0,predictions.shape[1]):
-        results[i,:,:,:]=np.sum(np.abs(predictions[i,:,:,:] - truth[i,:,:,:])) / np.sum(np.abs(truth[i,:,:,:]))
+    results=np.zeros(predictions.shape[0])
+    for i in range(0,predictions.shape[0]):
+        results[i]=np.sum(np.abs(predictions[i,:,:,:] - truth[i,:,:,:])) / np.sum(np.abs(truth[i,:,:,:]))
+    return results
 
+def error_distribution(truth,predictions,nbins=20):
+    '''
+    'param truth: normalized ground truth, targets as [n_samples,64,64,3]
+    'param predictions: normalized output of network, predictions as [n_samples,64,64,3]
+    'return: nothing (plots relative error distributions)
+    '''
+    errors=relative_error_multiple(truth,predictions)
+    plt.hist(errors,nbins)
+    plt.xlabel('relative error')
+    plt.ylabel('occurences')
+    plt.title('mean = '+str(np.mean(errors))[0:5]+', min = '+str(np.min(errors))[0:5]+', max = '+str(np.max(errors))[0:5])
+    plt.show()
 
 
 # normalize data