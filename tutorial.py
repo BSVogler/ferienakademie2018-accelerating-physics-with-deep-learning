54446ee2e3aa4cecc0278f817feec4d3f6af8c1f
BSVogler <mail@benediktsvogler.com>
2018-10-02 09:59:29 +0200

    renamed method

--- tutorial.py
+++ tutorial.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
         ground_truth[i] = d[3:6]  # p, velx, vely
     # print("inputs shape = ", inputs.shape)
 
-    inputs, ground_truth = preprocess_data(inputs, ground_truth)
+    inputs, ground_truth = normalize_data(inputs, ground_truth)
 
     # reorder because the channels must be the last dimension
     inputs = inputs.transpose(0, 2, 3, 1)
