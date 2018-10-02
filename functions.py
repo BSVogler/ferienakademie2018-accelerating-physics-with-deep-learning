e4b04d95d272034c9929f8a9a322dd421b984645
Peter <pkicsiny@gmail.com>
2018-10-02 11:05:32 +0200

    masked inf in plotter

--- functions.py
+++ functions.py
@@ -108,25 +108,28 @@ def plotter(predictionset, ground_truth, index=-1):
     eps = 1e-6
     plt.subplot(331)
     plt.title('Rel. error pressure', fontsize=10)
-    plt.imshow(np.abs(ground_truth[sampleindex, :, :, 0] - predictionset[random_sample, :, :, 0]) / np.abs(
-        ground_truth[sampleindex, :, :, 0] + eps),
-               cmap='jet')
+    relerr = np.abs(ground_truth[sampleindex, :, :, 0]-predictionset[random_sample, :, :, 0]) /\
+             np.abs(ground_truth[sampleindex,:, :, 0])
+    relerrmask = np.ma.masked_where(relerr == np.Inf, relerr)
+    plt.imshow(relerrmask,cmap='jet')
     plt.colorbar()
     plt.axis('off')
 
     plt.subplot(332)
     plt.title('Rel. error x velocity', fontsize=10)
-    plt.imshow(np.abs(ground_truth[sampleindex, :, :, 1] - predictionset[random_sample, :, :, 1]) / np.abs(
-        ground_truth[sampleindex, :, :, 1] + eps),
-               cmap='jet')
+    relerr = np.abs(ground_truth[sampleindex, :, :, 1]-predictionset[random_sample, :, :, 1]) /\
+             np.abs(ground_truth[sampleindex,:, :, 1])
+    relerrmask = np.ma.masked_where(relerr == np.Inf, relerr)
+    plt.imshow(relerrmask,cmap='jet')
     plt.colorbar()
     plt.axis('off')
 
     plt.subplot(333)
     plt.title('Rel. error y velocity', fontsize=10)
-    plt.imshow(np.abs(ground_truth[sampleindex, :, :, 2] - predictionset[random_sample, :, :, 2]) / np.abs(
-        ground_truth[sampleindex, :, :, 2] + eps),
-               cmap='jet')
+    relerr = np.abs(ground_truth[sampleindex, :, :, 2]-predictionset[random_sample, :, :, 2]) /\
+             np.abs(ground_truth[sampleindex,:, :, 2])
+    relerrmask = np.ma.masked_where(relerr == np.Inf, relerr)
+    plt.imshow(relerrmask,cmap='jet')
     plt.colorbar()
     plt.axis('off')
 
